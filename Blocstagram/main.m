178b11174b315dc80a277657f4eb4abc3cc5e022
startupguysandy <andy@thestartupguys.co.uk>
2014-09-01 21:45:52 +0100

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Andy Bradbury on 01/09/2014.
+//  Copyright (c) 2014 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
