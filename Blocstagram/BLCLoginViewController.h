19649a8be293dd77ff551785d738e4080620985c
startupguysandy <andy@thestartupguys.co.uk>
2014-09-23 14:16:45 -0400

    Add Instagram Login

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Andy Bradbury on 23/09/2014.
+//  Copyright (c) 2014 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+extern NSString *const BLCLoginViewControllerDidGetAccessTokenNotification;
+
+@end
---
+++ Blocstagram/BLCLoginViewController.m
@@ -0,0 +1,98 @@
+//
+//  BLCLoginViewController.m
+//  Blocstagram
+//
+//  Created by Andy Bradbury on 23/09/2014.
+//  Copyright (c) 2014 Tin Bear Studios. All rights reserved.
+//
+
+#import "BLCLoginViewController.h"
+#import "BLCDataSource.h"
+
+@interface BLCLoginViewController () <UIWebViewDelegate>
+
+@property (nonatomic, weak) UIWebView *webView;
+
+@end
+
+@implementation BLCLoginViewController
+
+NSString *const BLCLoginViewControllerDidGetAccessTokenNotification = @"BLCLoginViewControllerDidGetAccessTokenNotification";
+
+- (NSString *)redirectURI {
+    return @"http://beartownstudio.com";
+}
+
+- (void)loadView {
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.delegate = self;
+    
+    self.webView = webView;
+    self.view = webView;
+    
+    self.title = @"Login";
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [BLCDataSource instagramClientID], [self redirectURI]];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if (url) {
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+    }
+}
+
+- (void) dealloc {
+    // Removing this line causes a weird flickering effect when you relaunch the app after logging in, as the web view is briefly displayed, automaticall authenticates with cookies, returns the access token, and dismisses the login view, sometimes in less than a second.
+    [self clearInstagramCookies];
+    
+    // See https://developer.apple.com/library/ios/documentation/uikit/reference/UIWebViewDelegate_Protocol/Reference/Reference.html#//apple_ref/doc/uid/TP40006951-CH3-DontLinkElementID_1
+    
+    self.webView.delegate = nil;
+}
+
+/**
+ Clears Instagram cookies. This prevents caching the credentials in the cookie jar.
+ */
+- (void) clearInstagramCookies {
+    for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        NSRange domainRange = [cookie.domain rangeOfString:@"instagram.com"];
+        if (domainRange.location != NSNotFound) {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+        }
+    }
+}
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *urlString = request.URL.absoluteString;
+    if ([urlString hasPrefix:[self redirectURI]]) {
+        // This contains our auth token
+        NSRange rangeOfAccessTokenParameter = [urlString rangeOfString:@"access_token="];
+        NSUInteger indexOfTokenStarting = rangeOfAccessTokenParameter.location + rangeOfAccessTokenParameter.length;
+        NSString *accessToken = [urlString substringFromIndex:indexOfTokenStarting];
+        [[NSNotificationCenter defaultCenter] postNotificationName:BLCLoginViewControllerDidGetAccessTokenNotification object:accessToken];
+        return NO;
+    }
+    return YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
