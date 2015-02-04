5d1a28b2ae7aff0fe89321143ca2ee00e8c9823c
startupguysandy <andy@thestartupguys.co.uk>
2015-02-04 21:18:56 +0000

    Photo filters and sending to Instagram

--- Blocstagram/BLCImageLibraryViewController.m
+++ Blocstagram/BLCImageLibraryViewController.m
@@ -231,24 +231,7 @@ static NSString * const reuseIdentifier = @"Cell";
 
 #pragma mark <UICollectionViewDataSource>
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete method implementation -- Return the number of sections
-    return 0;
-}
 
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete method implementation -- Return the number of items in the section
-    return 0;
-}
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell
-    
-    return cell;
-}
 
 #pragma mark <UICollectionViewDelegate>
 
---
+++ Blocstagram/BLCPostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCPostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Andy Bradbury on 30/01/2015.
+//  Copyright (c) 2015 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCPostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
